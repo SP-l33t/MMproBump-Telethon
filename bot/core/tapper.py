7b4e5c705a777ee8b38fb53e1c3f33583d779da4
Slayer <slayerpro27@gmail.com>
2024-10-01 21:28:53 +0300

    Reinvented locking system by making it async

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -1,8 +1,5 @@
-from datetime import timedelta
-
 import aiohttp
 import asyncio
-import fasteners
 import hashlib
 import os
 import random
@@ -10,17 +7,18 @@ from urllib.parse import unquote
 from aiocfscrape import CloudflareScraper
 from aiohttp_proxy import ProxyConnector
 from better_proxy import Proxy
+from datetime import timedelta
 from time import time
 
 from telethon import TelegramClient
 from telethon.errors import *
-from telethon.types import InputUser, InputPeerUser
+from telethon.types import InputPeerUser
 from telethon.functions import messages, contacts
 
 from .agents import generate_random_user_agent
 from .headers import *
 from bot.config import settings
-from bot.utils import logger, log_error, proxy_utils, config_utils, CONFIG_PATH
+from bot.utils import logger, log_error, proxy_utils, config_utils, AsyncInterProcessLock, CONFIG_PATH
 from bot.exceptions import InvalidSession
 
 
@@ -34,11 +32,9 @@ class Tapper:
         self.session_name, _ = os.path.splitext(os.path.basename(tg_client.session.filename))
         self.tg_client = tg_client
         self.config = config_utils.get_session_config(self.session_name, CONFIG_PATH)
-        self.proxy = self.config.get('proxy', None)
-        self.lock = fasteners.InterProcessLock(os.path.join(os.path.dirname(CONFIG_PATH), 'lock_files',  f"{self.session_name}.lock"))
+        self.proxy = self.config.get('proxy')
+        self.lock = AsyncInterProcessLock(os.path.join(os.path.dirname(CONFIG_PATH), 'lock_files',  f"{self.session_name}.lock"))
         self.headers = headers
-        self.headers['User-Agent'] = self.check_user_agent()
-        self.headers.update(**get_sec_ch_ua(self.headers.get('User-Agent', '')))
 
         self._webview_data = None
 
@@ -50,14 +46,15 @@ class Tapper:
     def log_message(self, message) -> str:
         return f"<light-yellow>{self.session_name}</light-yellow> | {message}"
 
-    def check_user_agent(self):
+    async def check_user_agent(self):
         user_agent = self.config.get('user_agent')
         if not user_agent:
             user_agent = generate_random_user_agent()
             self.config['user_agent'] = user_agent
-            config_utils.update_session_config_in_file(self.session_name, self.config, CONFIG_PATH)
+            await config_utils.update_session_config_in_file(self.session_name, self.config, CONFIG_PATH)
 
-        return user_agent
+        self.headers['User-Agent'] = user_agent
+        self.headers.update(**get_sec_ch_ua(user_agent))
 
     async def initialize_webview_data(self):
         if not self._webview_data:
@@ -81,7 +78,7 @@ class Tapper:
 
     async def get_tg_web_data(self) -> [str | None, str | None]:
         data = None, None
-        with self.lock:
+        async with self.lock:
             try:
                 if not self.tg_client.is_connected():
                     await self.tg_client.connect()
@@ -125,7 +122,7 @@ class Tapper:
             finally:
                 if self.tg_client.is_connected():
                     await self.tg_client.disconnect()
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(15)
 
         return data
 
@@ -361,6 +358,7 @@ class Tapper:
             await asyncio.sleep(delay=3)
 
     async def run(self) -> None:
+        await self.check_user_agent()
         random_delay = random.randint(1, settings.RANDOM_DELAY_IN_RUN)
         logger.info(self.log_message(f"Bot will start in <ly>{random_delay}s</ly>"))
         await asyncio.sleep(random_delay)
@@ -465,7 +463,7 @@ class Tapper:
                         else:
                             sleep_time = sleep_time if time_left > 3600 else time_left
                             logger.info(self.log_message(
-                                f"Farming in progress, <light-yellow>{round(time_left / 60, 1)}</light-yellow> min before end"))
+                                f"Farming in progress, <ly>{round(time_left / 60, 1)}</ly> min before end"))
 
                     logger.info(self.log_message(f"Sleep {sleep_time} seconds"))
                     await asyncio.sleep(delay=sleep_time)
@@ -484,6 +482,3 @@ async def run_tapper(tg_client: TelegramClient):
         await runner.run()
     except InvalidSession as e:
         log_error(runner.log_message(f"Invalid Session: {e}"))
-    finally:
-        if runner.lock.acquired:
-            runner.lock.release()
